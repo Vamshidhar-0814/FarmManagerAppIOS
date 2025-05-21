d8f9accd9cd1961ba568abf5222bd236f3fadc86
Vamshidhar Reddy <vamshidharreddy0814@gmail.com>
2025-05-21 11:07:16 +0530

    Initial Commit

---
+++ FarmManager/FarmManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarmManagerApp.swift
+//  FarmManager
+//
+//  Created by Vamshidhar Reddy on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct FarmManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FarmManagerTests/FarmManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FarmManagerTests.swift
+//  FarmManagerTests
+//
+//  Created by Vamshidhar Reddy on 21/05/25.
+//
+
+import Testing
+@testable import FarmManager
+
+struct FarmManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
